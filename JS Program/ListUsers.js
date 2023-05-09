8524c1c68f2192bf7412d6c85e5b4d621ff2ebd4
Viktoriya <vika.pavlenko.161105@gmail.com>
2023-05-09 08:41:08 +0300

    Traversed the array and filed in the table.

--- JS Program/ListUsers.js
+++ JS Program/ListUsers.js
@@ -1,25 +1,23 @@
-const modal = document.querySelector(".modal");
-const trigger = document.querySelector(".trigger");
-const closeButton = document.querySelector(".close-button");
-const cancelButton = document.querySelector(".cancel-button");
-
-function toggleModal() {
-  modal.classList.toggle("show-modal");
-}
-
-function windowOnClick(event) {
-  if (event.target === modal) {
-    toggleModal();
-  }
-}
-
-trigger.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
-cancelButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
-
 function loadUsers() {
-   fetch('ListUsers.json')
-     .then(response => response.json())
-     .then(data => console.log(data));
-}
+  fetch('ListUsers.json')
+    .then(response => response.json())
+    .then(data => {
+      const table = document.querySelector('#table');
+      const tbody = table.querySelector('tbody');
+      for (const user of data) {
+        const row = tbody.insertRow();
+        const idCell = row.insertCell();
+        idCell.textContent = user.id;
+        const nameCell = row.insertCell();
+        nameCell.textContent = `${user.firstName} ${user.lastName}`;
+        const birthdayCell = row.insertCell();
+        birthdayCell.textContent = user.birthday;
+        const cityCell = row.insertCell();
+        cityCell.textContent = user.city;
+        const actionsCell = row.insertCell();
+        actionsCell.innerHTML = '<a title="Edit"><button class="trigger" onclick="editUsers"><img src="img/img.png" style="width: 15px; height: 15px;"></a><a title="Delete"></button><button class="trigger" onclick="deleteUsers"><img src="img/imgX.jpeg" style="width: 15px; height: 15px;"></button></a>';
+      }
+    })
+};
+
+loadUsers();
