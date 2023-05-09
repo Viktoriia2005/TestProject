a13644e90a271a17a0dedc5f9fe752e479af2764
serhiy.krasovskyy <xhunter74@gmail.com>
2023-05-09 12:31:28 +0300

    Optimized code.

--- JS Program/ListUsers.js
+++ JS Program/ListUsers.js
@@ -5,19 +5,19 @@ function loadUsers() {
       const table = document.querySelector('#table');
       const tbody = table.querySelector('tbody');
       for (const user of data) {
-        const row = tbody.insertRow();
-        const idCell = row.insertCell();
-        idCell.textContent = user.id;
-        const nameCell = row.insertCell();
-        nameCell.textContent = `${user.firstName} ${user.lastName}`;
-        const birthdayCell = row.insertCell();
-        birthdayCell.textContent = user.birthday;
-        const cityCell = row.insertCell();
-        cityCell.textContent = user.city;
-        const actionsCell = row.insertCell();
-        actionsCell.innerHTML = '<a title="Edit"><button class="trigger" onclick="editUsers"><img src="img/img.png" style="width: 15px; height: 15px;"></a><a title="Delete"></button><button class="trigger" onclick="deleteUsers"><img src="img/imgX.jpeg" style="width: 15px; height: 15px;"></button></a>';
+        const newRow = tbody.insertRow();
+        let newCell = newRow.insertCell();
+        newCell.textContent = user.id;
+        newCell = newRow.insertCell();
+        newCell.textContent = `${user.firstName} ${user.lastName}`;
+        newCell = newRow.insertCell();
+        newCell.textContent = user.birthday;
+        newCell = newRow.insertCell();
+        newCell.textContent = user.city;
+        newCell = newRow.insertCell();
+        newCell.innerHTML = '<a title="Edit"><button class="trigger" onclick="editUsers"><img src="img/img.png" style="width: 15px; height: 15px;"></a><a title="Delete"></button><button class="trigger" onclick="deleteUsers"><img src="img/imgX.jpeg" style="width: 15px; height: 15px;"></button></a>';
       }
     })
 };
 
-loadUsers();
+// loadUsers();
