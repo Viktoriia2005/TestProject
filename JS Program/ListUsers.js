644a1e74b6671083df45e1cc0f617e1a07ae7143
Viktoriya <vika.pavlenko.161105@gmail.com>
2023-05-10 19:05:30 +0300

    Redid the code where up to 4 people are included without adding a line and blocked the Load button with a message that no more can be added. Made a date in the modal window.

--- JS Program/ListUsers.js
+++ JS Program/ListUsers.js
@@ -4,24 +4,27 @@ function loadUsers() {
     .then(data => {
       const table = document.querySelector('#table');
       const tbody = table.querySelector('tbody');
-      if (tbody.rows.length >= 4) {
-        return;
+      if (tbody.rows.length < 4) {
+        const button = document.querySelector('#myButton');
+        button.disabled = true;
+        alert('Ви не можете додавати більше користувачів!');
       }
       for (const user of data) {
-        const newRow = tbody.insertRow();
-        let newCell = newRow.insertCell();
-        newCell.textContent = user.id;
-        newCell = newRow.insertCell();
-        newCell.textContent = `${user.firstName} ${user.lastName}`;
-        newCell = newRow.insertCell();
-        newCell.textContent = new Date(user.birthday).toLocaleDateString('uk-UA');
-        newCell = newRow.insertCell();
-        newCell.textContent = user.city;
-        newCell = newRow.insertCell();
-        newCell.innerHTML = '<div class="edit-delet-text"><a title="Edit"><button class="btn btn-info" onclick="editUsers"><span class="material-symbols-outlined">edit</span></a></button><a title="Delete"><button class="btn btn-info" onclick="deleteUsers"><span class="material-symbols-outlined">delete</span></button></a></div>';
+        const row = tbody.insertRow();
+        const idCell = row.insertCell();
+        idCell.textContent = user.id;
+        const nameCell = row.insertCell();
+        nameCell.textContent = `${user.firstName} ${user.lastName}`;
+        const birthdayCell = row.insertCell();
+        birthdayCell.textContent = new Date(user.birthday).toLocaleDateString('uk-UA');
+        const cityCell = row.insertCell();
+        cityCell.textContent = user.city;
+        const actionsCell = row.insertCell();
+        actionsCell.innerHTML = '<div class="edit-delet-text"><a title="Edit"><button class="btn btn-info" onclick="editUsers"><span class="material-symbols-outlined">edit</span></a></button><a title="Delete"><button class="btn btn-info" onclick="deleteUsers"><span class="material-symbols-outlined">delete</span></button></a></div>';
       }
-    })
-};
+    });
+}
+
 
 function editUsers() {
   // для редагування користувача
@@ -31,11 +34,16 @@ function deleteUsers() {
   // для видалення користувача
 }
 
-let table = document.getElementById("myTable");
-let row = table.insertRow(-1);
-let cell1 = row.insertCell(0);
-let cell2 = row.insertCell(1);
-let cell3 = row.insertCell(2);
-cell1.innerHTML = document.getElementsByName("day")[0].value + "." + document.getElementsByName("month")[0].value + "." + document.getElementsByName("year")[0].value;
-cell2.innerHTML = ".";
-cell3.innerHTML = ".";
+$(document).ready(function () {
+
+  let userLang = navigator.language || navigator.userLanguage;
+
+  let options = $.extend({},
+    $.datepicker.regional[userLang], {
+    dateFormat: "dd.mm.yy"
+  }
+  );
+
+  $("#calendar").datepicker(options);
+});
+
