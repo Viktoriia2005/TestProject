e7f03a99fe23f8e5bdb62d236ff61fe602bfc823
serhiy.krasovskyy <xhunter74@gmail.com>
2023-06-06 15:34:24 +0300

    reworked.

--- Web Server/home.js
+++ Web Server/home.js
@@ -1,25 +1,9 @@
-const http = require('http');
+function sendResponse(res, statusCode, contentType, content) {
+    res.writeHead(statusCode, { 'Content-Type': contentType });
+    res.end(content);
+};
 
-const server = http.createServer((req, res) => {
-    // Function to set the response content and end the response
-    const sendResponse = (statusCode, contentType, content) => {
-        res.writeHead(statusCode, { 'Content-Type': contentType });
-        res.end(content);
-    };
-
-    // Function to handle the home page
-    const handleHome = () => {
-        const content = '<html><body><p>This is home Page.</p></body></html>';
-        sendResponse(200, 'text/html', content);
-    };
-    // Route handling
-    if (req.url === '/') {
-        handleHome();
-    }
-    else {
-        sendResponse(404, 'text/plain', 'Invalid Request!');
-    }
-});
-
-server.listen(5000);
-console.log('Node.js web server at port 5000 is running..');
+export function handleHome(res) {
+    const content = '<html><body><p>This is home Page.</p></body></html>';
+    sendResponse(res, 200, 'text/html', content);
+}
--- Web Server/index.js
+++ Web Server/index.js
@@ -1,56 +1,51 @@
-const http = require('http');
-const fs = require('fs');
+import {handleHome} from './home.js';
+import * as http from 'http';
+import * as fs from 'fs';
 
 const server = http.createServer((req, res) => {
     // Function to set the response content and end the response
-    const sendResponse = (statusCode, contentType, content) => {
-        res.writeHead(statusCode, { 'Content-Type': contentType });
-        res.end(content);
-    };
 
     // Function to handle the home page
-    const handleHome = () => {
-        const content = '<html><body><p>This is home Page.</p></body></html>';
-        sendResponse(200, 'text/html', content);
-    };
+
 
     // Function to handle the student page
-    const handleStudent = () => {
-        const content = '<html><body><p>This is student Page.</p></body></html>';
-        sendResponse(200, 'text/html', content);
-    };
+    // const handleStudent = () => {
+    //     const content = '<html><body><p>This is student Page.</p></body></html>';
+    //     sendResponse(200, 'text/html', content);
+    // };
 
-    // Function to handle the admin page
-    const handleAdmin = () => {
-        const content = '<html><body><p>This is admin Page.</p></body></html>';
-        sendResponse(200, 'text/html', content);
-    };
+    // // Function to handle the admin page
+    // const handleAdmin = () => {
+    //     const content = '<html><body><p>This is admin Page.</p></body></html>';
+    //     sendResponse(200, 'text/html', content);
+    // };
 
-    // Function to handle the users page
-    const handleUsers = () => {
-        fs.readFile('SiteData.json', 'utf8', (err, data) => {
-            if (err) {
-                console.error('Помилка при читанні файлу:', err);
-                sendResponse(500, 'text/plain', 'Internal Server Error');
-                return;
-            }
+    // // Function to handle the users page
+    // const handleUsers = () => {
+    //     fs.readFile('SiteData.json', 'utf8', (err, data) => {
+    //         if (err) {
+    //             console.error('Помилка при читанні файлу:', err);
+    //             sendResponse(500, 'text/plain', 'Internal Server Error');
+    //             return;
+    //         }
 
-            sendResponse(200, 'application/json', data);
-        });
-    };
+    //         sendResponse(200, 'application/json', data);
+    //     });
+    // };
 
     // Route handling
     if (req.url === '/') {
-        handleHome();
-    } else if (req.url === '/student') {
-        handleStudent();
-    } else if (req.url === '/admin') {
-        handleAdmin();
-    } else if (req.url === '/users') {
-        handleUsers();
-    } else {
-        sendResponse(404, 'text/plain', 'Invalid Request!');
+        handleHome(res);
     }
+    // else if (req.url === '/student') {
+    //     handleStudent();
+    // } else if (req.url === '/admin') {
+    //     handleAdmin();
+    // } else if (req.url === '/users') {
+    //     handleUsers();
+    // } else {
+    //     sendResponse(404, 'text/plain', 'Invalid Request!');
+    // }
 });
 
 server.listen(5000);
