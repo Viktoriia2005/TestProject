ad33943e4e362d3ce84877717f8cccc68623275a
vika_pavlenko_161105 <vika.pavlenko.161105@gmail.com>
2023-06-07 09:14:19 +0300

    Fixed issues

--- Web Server/main.js
+++ Web Server/main.js
@@ -1,53 +1,12 @@
 import { handleHome } from './home.js';
 import * as http from 'http';
-import * as fs from 'fs';
-
 import { handleStudent } from './student.js';
-import * as http from 'http';
-import * as fs from 'fs';
-
 import { handleAdmin } from './admin.js';
-import * as http from 'http';
-import * as fs from 'fs';
-
 import { handleUsers } from './users.js';
-import * as http from 'http';
-import * as fs from 'fs';
-
 import { handleCalc } from './calculator.js';
-import * as http from 'http';
-import * as fs from 'fs';
+
 
 const server = http.createServer((req, res) => {
-    // Function to set the response content and end the response
-
-    // Function to handle the home page
-
-
-    // Function to handle the student page
-    // const handleStudent = () => {
-    //     const content = '<html><body><p>This is student Page.</p></body></html>';
-    //     sendResponse(200, 'text/html', content);
-    // };
-
-    // // Function to handle the admin page
-    // const handleAdmin = () => {
-    //     const content = '<html><body><p>This is admin Page.</p></body></html>';
-    //     sendResponse(200, 'text/html', content);
-    // };
-
-    // // Function to handle the users page
-    // const handleUsers = () => {
-    //     fs.readFile('SiteData.json', 'utf8', (err, data) => {
-    //         if (err) {
-    //             console.error('Помилка при читанні файлу:', err);
-    //             sendResponse(500, 'text/plain', 'Internal Server Error');
-    //             return;
-    //         }
-
-    //         sendResponse(200, 'application/json', data);
-    //     });
-    // };
 
     // Route handling
     if (req.url === '/') {
