a1a35e8f13316dd2e879c4db489534014205b8a5
vika_pavlenko_161105 <vika.pavlenko.161105@gmail.com>
2023-06-08 15:47:52 +0300

    Made changes to the code.

--- Web Server/main.js
+++ Web Server/main.js
@@ -8,26 +8,30 @@ import { handleCalc } from './calculator.js';
 import { sendResponse } from './utils.js';
 
 const server = http.createServer((req, res) => {
-    const requestUrl = url.parse(req.url, true);
+    try {
+        const requestUrl = url.parse(req.url, true);
 
-    // Route handling
-    if (req.url === '/' && req.method === 'GET') {
-        handleHome(res);
-    }
-    else if (req.url === '/student' && req.method === 'GET') {
-        handleStudent(res);
-    }
-    else if (req.url === '/admin' && req.method === 'GET') {
-        handleAdmin(res);
-    }
-    else if (req.url === '/users' && req.method === 'GET') {
-        handleUsers(res);
-    }
-    else if (requestUrl.pathname === '/calc' && req.method === 'POST') {
-        handleCalc(req, res);
-    }    
-    else {
-        sendResponse(res, 404, 'text/plain', 'Invalid Request!');
+        // Route handling
+        if (requestUrl.pathname === '/' && req.method === 'GET') {
+            handleHome(res);
+        }
+        else if (requestUrl.pathname === '/student' && req.method === 'GET') {
+            handleStudent(res);
+        }
+        else if (requestUrl.pathname === '/admin' && req.method === 'GET') {
+            handleAdmin(res);
+        }
+        else if (requestUrl.pathname === '/users' && req.method === 'GET') {
+            handleUsers(res);
+        }
+        else if (requestUrl.pathname === '/calc' && req.method === 'POST') {
+            handleCalc(req, res);
+        }
+        else {
+            sendResponse(res, 404, 'text/plain', 'Invalid Request!');
+        }
+    } catch (error) {
+        sendResponse(res, 500, 'text/html', error);
     }
 });
 
