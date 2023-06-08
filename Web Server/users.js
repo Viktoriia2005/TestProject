bc3c813458ee1304afde276183a194a8cfef0b7e
vika_pavlenko_161105 <vika.pavlenko.161105@gmail.com>
2023-06-08 15:49:19 +0300

    Made it so that users were extracted from the array.

--- Web Server/users.js
+++ Web Server/users.js
@@ -1,14 +1,22 @@
 import { sendResponse } from "./utils.js";
+import fs from 'fs';
 
+export function handleUsers(res) {
+    fs.readFile('SiteData.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            sendResponse(res, 500, 'text/plain', 'Internal Server Error');
+            return;
+        }
 
-    export function handleUsers (res) {
-        fs.readFile('SiteData.json', 'utf8', (err, data) => {
-            if (err) {
-                console.error('Error reading file:', err);
-                sendResponse(res, 500, 'text/plain', 'Internal Server Error');
-                return;
-            }
+        try {
+            const jsonData = JSON.parse(data);
+            const usersArray = jsonData.users;
 
-            sendResponse(res, 200, 'application/json', data);
-        });
-    }
+            sendResponse(res, 200, 'application/json', JSON.stringify(usersArray));
+        } catch (error) {
+            console.error('Error parsing JSON:', error);
+            sendResponse(res, 500, 'text/plain', 'Internal Server Error');
+        }
+    });
+}
