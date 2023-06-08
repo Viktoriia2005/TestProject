8ce99d212340f5bcf15833cec048eb554307267b
vika_pavlenko_161105 <vika.pavlenko.161105@gmail.com>
2023-06-08 15:46:42 +0300

    Reacted and adds new elements to the code.

--- Web Server/calculator.js
+++ Web Server/calculator.js
@@ -1,20 +1,25 @@
 import { sendResponse } from "./utils.js";
-
+import * as url from 'url';
 
 export function handleCalc(req, res) {
-    const param1 = parseInt(req.url.split('=')[1]);
-    let body = '';
+    const requestUrl = url.parse(req.url, true);
 
-    req.on('data', (chunk) => {
-        body += chunk;
-    }).on('end', () => {
-        const data = JSON.parse(body);
-        const param2 = data.param2;
-        const result = param1 + param2;
-        const response = JSON.stringify({ result });
+    if (requestUrl.query.param1) {
 
-        sendResponse(res, 200, 'application/json', response);
+        let body = '';
 
-    });
+        req.on('data', (chunk) => {
+            body += chunk;
+        }).on('end', () => {
+            const data = JSON.parse(body);
+            const result = Number(requestUrl.query.param1) + data.param2;
+            const response = JSON.stringify({ result });
 
+            sendResponse(res, 200, 'application/json', response);
+
+        });
+    }
+    else {
+        sendResponse(res, 500, 'text/html', 'Parameter param1 is required');
+    }
 };
