35cf4f6ae64b5e7c1b83eb222ba26c3261cbc994
serhiy.krasovskyy <xhunter74@gmail.com>
2023-06-14 09:30:25 +0300

    Reverted changes.

--- JS Program/ListUsers.js
+++ JS Program/ListUsers.js
@@ -1,26 +1,15 @@
-const express = require('express');
-const fs = require('fs');
-const bodyParser = require('body-parser');
-
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 let users;
 let cities;
 
-function loadUsers(req, res) {
-  fs.readFile('SiteData.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error reading file:', err);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
-
-    try {
-      const jsonData = JSON.parse(data);
-      users = jsonData.users;
-      cities = jsonData.cities;
+function loadUsers() {
+  const button = document.querySelector('#myButton');
+  button.disabled = true;
+  button.textContent = 'Loading...';
+  fetch('SiteData.json')
+    .then(response => response.json())
+    .then(data => {
+      users = data.users;
+      cities = data.cities;
       cities.sort((a, b) => a.name.localeCompare(b.name));
       const table = document.querySelector('#table');
       const tbody = table.querySelector('tbody');
@@ -40,10 +29,9 @@ function loadUsers(req, res) {
         let cityData = cities.find(city => city.id === user.city);
         cityCell.textContent = cityData ? cityData.name : '';
         const actionsCell = row.insertCell();
-        actionsCell.innerHTML = `<div class="edit-delet-text"><a title="Edit"><button data-bs-toggle="modal" data-bs-target="#editUserModal" class="btn btn-info" id="Edit" onclick="showEditUserPopup(${user.id})"><span class="material-symbols-outlined">edit</span></button></a><a title="Delete"><button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#staticBackdrop" id="Delete" onclick="showDeleteUserPopup(${user.id})"><span class="material-symbols-outlined">delete</span></button></a></div>`;
+        actionsCell.innerHTML = `<div class="edit-delet-text"><a title="Edit"><button data-bs-toggle="modal" data-bs-target="#editUserModal" class="btn btn-info" id = "Edit" onclick="showEditUserPopup(${user.id})"><span class="material-symbols-outlined">edit</span></a></button><a title="Delete"><button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#staticBackdrop" id = "Delete" onclick="showDeleteUserPopup(${user.id})"><span class="material-symbols-outlined">delete</span></button></a></div>`;
       }
 
-      const button = document.querySelector('#buttonModale');
       button.disabled = true;
       button.textContent = 'Cannot add users';
 
@@ -53,7 +41,6 @@ function loadUsers(req, res) {
       addButton.addEventListener('click', addUser);
 
       let select = document.getElementById('cityInput');
-      select.innerHTML = ''; // Очистити вміст select перед додаванням опцій
 
       cities.forEach(city => {
         let option = document.createElement('option');
@@ -62,39 +49,76 @@ function loadUsers(req, res) {
 
         select.appendChild(option);
       });
+    })
 
-      // Надіслати успішну відповідь на завантаження даних
-      res.status(200).send('Data loaded successfully');
-    } catch (error) {
-      console.error('Error parsing JSON:', error);
-      res.status(500).send('Internal Server Error');
+};
+
+
+function getMaxId() {
+  let maxId = 0;
+  for (const user of users) {
+    if (user.id > maxId) {
+      maxId = user.id;
     }
-  });
+  }
+  return maxId;
 }
 
-app.post("/users", function (req, res) {
-  const name = req.body.name;
-  const birthday = req.body.birthday;
-  const city = req.body.city;
-
-  function getMaxId() {
-    let maxId = 0;
-    for (const user of users) {
-      if (user.id > maxId) {
-        maxId = user.id;
-      }
-    }
-    return maxId;
-  }
-
+function addUser() {
+  const name = document.querySelector('#nameInput').value;
+  const birthday = document.querySelector('#birthdayInput').value;
+  const city = document.querySelector('#cityInput').value;
+  const tmp = document.getElementById("buttonModale");
+  console.log(tmp);
   const maxId = getMaxId();
   const id = maxId + 1;
 
-  const newUser = { id, name, birthday, city };
+  const tableRef = document.getElementById('table').getElementsByTagName('tbody')[0];
+  const newRow = tableRef.insertRow();
+  newRow.id = 'user' + id;
+
+  const newCell = newRow.insertCell(0);
+  const newText = document.createTextNode(id);
+  newCell.appendChild(newText);
+
+  const newCell2 = newRow.insertCell(1);
+  newCell2.setAttribute('name', 'userName');
+  const newText2 = document.createTextNode(name);
+  newCell2.appendChild(newText2);
+
+  const newCell3 = newRow.insertCell(2);
+  newCell3.setAttribute('name', 'userBirthday');
+  const newText3 = document.createTextNode(birthday);
+  newCell3.appendChild(newText3);
+
+  const newCell4 = newRow.insertCell(3);
+  newCell4.setAttribute('name', 'userCity');
+  const cityId = Number(city);
+  const cityData = cities.find(city => city.id === cityId);
+  const newText4 = document.createTextNode(cityData.name);
+  newCell4.appendChild(newText4);
+
+  // Update maxId value
+  const updatedMaxId = maxId + 1;
+
+  // Update users with a new user
+  const newUser = { id: updatedMaxId, name, birthday, city };
   users.push(newUser);
 
-  res.status(200).send("User added successfully");
-});
+  // Reset Input Field Values
+  document.querySelector('#nameInput').value = '';
+  document.querySelector('#birthdayInput').value = '';
+  document.querySelector('#cityInput').value = '';
+
+  // Update row id of table
+  newRow.setAttribute('id', 'userRow-' + updatedMaxId);
+
+  // Add action buttons for a new user
+  const actionsCell = newRow.insertCell();
+  actionsCell.innerHTML = `<div class="edit-delet-text"><a title="Edit"><button data-bs-toggle="modal" data-bs-target="#editUserModal" class="btn btn-info" id="Edit" onclick="showEditUserPopup(${updatedMaxId})"><span class="material-symbols-outlined">edit</span></a></button><a title="Delete"><button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#staticBackdrop" id="Delete" onclick="showDeleteUserPopup(${updatedMaxId})"><span class="material-symbols-outlined">delete</span></button></a></div>`;
+  const modal = document.querySelector('#editUserModal');
+  $(modal).modal('hide');
+}
 
 function showEditUserPopup(userId) {
   const user = users.find(u => u.id === userId);
@@ -143,57 +167,89 @@ function downloadData() {
   dlAnchorElem.click();
 }
 
-// PUT update user
-app.put("/users/:id", function (req, res) {
-  const userId = parseInt(req.params.id);
-  const name = req.body.name;
-  const birthday = req.body.birthday;
-  const city = req.body.city;
-  const isAdmin = req.body.isAdmin;
-
-  function updateUser(userId, name, birthday, city, isAdmin) {
+function saveUser(userId) {
+  const nameInput = document.querySelector('#nameInput');
+  const birthdayInput = document.querySelector('#birthdayInput');
+  const cityInput = document.querySelector('#cityInput');
+  const isAdminInput = document.querySelector('#isAdminInput');
+  const table = document.querySelector('#table');
+  const row = table.rows[userId];
+  if (row) {
+    const nameCell = row.querySelector(`td[name="userName"]`);
+    nameCell.textContent = nameInput.value;
+    const birthdayCell = row.querySelector(`td[name="userBirthday"]`);
+    birthdayCell.textContent = new Date(birthdayInput.value).toLocaleDateString('uk-UA');
+    const cityCell = row.querySelector(`td[name="userCity"]`);
+    const cityId = parseInt(cityInput.value);
+    const city = cities.find(c => c.id === cityId);
+    if (city) {
+      cityCell.textContent = city.name;
+    } else {
+      cityCell.textContent = '';
+    }
     const user = users.find(u => u.id === userId);
-    if (user) {
-      user.name = name;
-      user.birthday = birthday;
-      user.city = city;
-      user.isAdmin = isAdmin;
-      return true;
-    }
-    return false;
+    user.name = nameInput.value;
+    user.birthday = birthdayInput.value;
+    user.city = cityInput.value;
+    user.isAdmin = isAdminInput.checked;
+    console.log(users);
   }
-
-  const isUpdated = updateUser(userId, name, birthday, city, isAdmin);
-  if (isUpdated) {
-    res.status(200).send("User updated successfully");
-  } else {
-    res.status(404).send("User not found");
+  else {
+    console.log(`Can not find user with id = ${userId}`);
   }
-});
+  const modal = document.querySelector('#editUserModal');
+  $(modal).modal('hide');
 
+}
 
+function showAddUserPopup() {
+  document.querySelector('#nameInput').value = '';
+  document.querySelector('#birthdayInput').value = '';
+  document.querySelector('#cityInput').value = '';
+  let addButton = document.querySelector('#buttonModale');
+  const newAddButton = addButton.cloneNode(true);
+  addButton.replaceWith(newAddButton);
+  newAddButton.addEventListener('click', () => addUser());
+  document.getElementById("buttonModale").textContent = "Add user";
+  document.getElementById("editModalUser").textContent = "Add new user";
+  let popup = document.getElementById("editUserModal");
+  popup.classList.toggle("show");
+}
 
-// DELETE user
-app.delete("/users/:id", function (req, res) {
-  const userId = parseInt(req.params.id);
+function showDeleteUserPopup(userId) {
+  const user = users.find(u => u.id === userId);
+  if (user) {
+    console.log(user);
 
-  function deleteUser(userId) {
-    const userIndex = users.findIndex(u => u.id === userId);
-    if (userIndex !== -1) {
-      users.splice(userIndex, 1);
-      return true;
-    }
-    return false;
+    const modalWindow = document.querySelector('#staticBackdrop');
+    const modalBody = modalWindow.querySelector('.modal-body');
+    modalBody.innerHTML = `<p>Do you really want to delete the ${user.name}?</p>`;
+
+    let deleteButton = document.querySelector('#popupDelete');
+    const newDeleteButton = deleteButton.cloneNode(true);
+    deleteButton.replaceWith(newDeleteButton);
+    newDeleteButton.addEventListener('click', () => deleteUser(userId));
+    let popup = document.getElementById("staticBackdrop");
+    popup.classList.toggle("show");
   }
-
-  const isDeleted = deleteUser(userId);
-  if (isDeleted) {
-    res.status(200).send("User deleted successfully");
-  } else {
-    res.status(404).send("User not found");
+  else {
+    console.log(`Can not find user with id = ${userId}`);
   }
-});
+}
 
+function deleteUser(userId) {
+  const userIndex = users.findIndex(u => u.id === userId);
+  if (userIndex !== -1) {
+    users.splice(userIndex, 1);
+    console.log(`User with id = ${userId} was deleted`);
+    const rowId = `userRow-${userId}`;
+    const row = document.getElementById(rowId);
+    row.remove();
+  }
+  else {
+    console.log(`Can not find user with id = ${userId}`);
+  }
+}
 
 $(document).ready(function () {
 
